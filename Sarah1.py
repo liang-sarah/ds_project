ad77213d5442024a03e72d683986d5a12c5c5e3c
Sarah Liang <86575752+liang-sarah@users.noreply.github.com>
2022-11-16 12:44:58 -0800

    Update and rename test1.py to Sarah1.py

---
+++ Sarah1.py
@@ -0,0 +1,11 @@
+## READ IN CSV DATA
+# to install pandas, type in terminal: python -m pip install pandas
+# restart vscode, or use idle then you should be able to import/use pandas
+
+import pandas as pd
+ 
+# reading the CSV file
+csvFile = pd.read_csv("C:\\Users\waliang\Documents\\py_data\\recipes_82k.csv")
+ 
+# displaying the contents of the CSV file
+print(csvFile)
